e55f93243ab1675ed21ac488dd675733e02ebd07
Lautaro Tresca <lautarotrescadesarrollador@gmail.com>
2022-10-27 19:57:02 -0300

    creacion de .env

--- src/Components/services/firebase/index.js
+++ src/Components/services/firebase/index.js
@@ -2,12 +2,12 @@ import { getFirestore } from 'firebase/firestore';
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBR0G85n70sys9nRZ4D0UygQSxxh6fqxiI",
-  authDomain: "mi-proyecto-de-react-7292d.firebaseapp.com",
-  projectId: "mi-proyecto-de-react-7292d",
-  storageBucket: "mi-proyecto-de-react-7292d.appspot.com",
-  messagingSenderId: "386981800111",
-  appId: "1:386981800111:web:957f527b1ea73cfcb64c5a"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket, 
+  messagingSenderId: process.env.REACT_APP_messagingSenderId, 
+  appId: process.env.REACT_APP_appId
 };
 
 const app = initializeApp(firebaseConfig); //Vinculo mi app con Firebase
